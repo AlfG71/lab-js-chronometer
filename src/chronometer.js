5d3cef6fa134a2ef9099298e44dbf35937a694e2
AlfG71 <56656187+AlfG71@users.noreply.github.com>
2023-09-06 10:49:01 -0700

    iteration 1 done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,10 +1,11 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    
   }
 
   getMinutes() {
